d201a7cb24f1bc626f5ddde634325068e73a30d1
manojkone9072 <konemanoj28@gmail.com>
2020-12-31 18:32:00 +0530

    dded

---
+++ BlurDetectionlibrary/src/main/java/com/example/blurdetectionlibrary/Util.java
@@ -0,0 +1,11 @@
+package com.example.blurdetectionlibrary;
+
+import android.util.Log;
+
+public class Util {
+
+    private static String TAG ="Hello";
+    public static void d(String msg){
+        Log.d(TAG, msg);
+    }
+}
